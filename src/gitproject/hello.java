0dbb9a006240f1cb62d6ec9d296cc97708f2036f
parkgeon0726 <hit102412@naver.com>
2025-03-20 14:15:27 +0900

    first commit

---
+++ src/gitproject/hello.java
@@ -0,0 +1,22 @@
+package gitproject;
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("첫 번째 숫자 입력: ");
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 숫자 입력: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("합계: " + (num1 + num2));
+        System.out.println("차이: " + (num1 - num2));
+        System.out.println("곱셈: " + (num1 * num2));
+        if(num2 != 0) {
+            System.out.println("나눗셈: " + ((double)num1 / num2));
+        } else {
+            System.out.println("0으로 나눌 수 없습니다.");
+        }
+    }
+}
+
